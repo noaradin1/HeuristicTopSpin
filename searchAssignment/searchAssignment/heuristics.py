5fe959dc25576b8b8b43ef04bdb1d307e4e8871f
noaradin1 <noaradi@post.bgu.ac.il>
2024-06-15 18:12:26 +0300

    Updating deepcubeA + trained bootstrapping model

--- searchAssignment/searchAssignment/heuristics.py
+++ searchAssignment/searchAssignment/heuristics.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from topspin import TopSpinState
+import torch.nn.functional as F
+
 class BaseHeuristic:
     def __init__(self, n=11, k=4):
         self._n = n
@@ -26,29 +28,112 @@ class BaseHeuristic:
         return gaps
 
 class HeuristicModel(nn.Module):
-    def __init__(self, input_dim):
-        super(HeuristicModel, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 64)
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc2 = nn.Linear(64, 32)
-        self.dropout2 = nn.Dropout(0.25)
-        self.fc3 = nn.Linear(32, 16)
-        self.fc4 = nn.Linear(16, 1)
+    def __init__(self, state_dim: int, one_hot_depth: int, h1_dim: int, resnet_dim: int, num_resnet_blocks: int,
+                 out_dim: int, batch_norm: bool):
+        super().__init__()
+        self.one_hot_depth: int = one_hot_depth
+        self.state_dim: int = state_dim
+        self.blocks = nn.ModuleList()
+        self.num_resnet_blocks: int = num_resnet_blocks
+        self.batch_norm = batch_norm
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = self.dropout1(x)
-        x = torch.relu(self.fc2(x))
-        x = self.dropout2(x)
-        x = torch.relu(self.fc3(x))
-        x = self.fc4(x)
+        # first two hidden layers
+        if one_hot_depth > 0:
+            self.fc1 = nn.Linear(self.state_dim * self.one_hot_depth, h1_dim)
+        else:
+            self.fc1 = nn.Linear(self.state_dim, h1_dim)
+
+        if self.batch_norm:
+            self.bn1 = nn.BatchNorm1d(h1_dim)
+
+        self.fc2 = nn.Linear(h1_dim, resnet_dim)
+
+        if self.batch_norm:
+            self.bn2 = nn.BatchNorm1d(resnet_dim)
+
+        # resnet blocks
+        for block_num in range(self.num_resnet_blocks):
+            if self.batch_norm:
+                res_fc1 = nn.Linear(resnet_dim, resnet_dim)
+                res_bn1 = nn.BatchNorm1d(resnet_dim)
+                res_fc2 = nn.Linear(resnet_dim, resnet_dim)
+                res_bn2 = nn.BatchNorm1d(resnet_dim)
+                self.blocks.append(nn.ModuleList([res_fc1, res_bn1, res_fc2, res_bn2]))
+            else:
+                res_fc1 = nn.Linear(resnet_dim, resnet_dim)
+                res_fc2 = nn.Linear(resnet_dim, resnet_dim)
+                self.blocks.append(nn.ModuleList([res_fc1, res_fc2]))
+
+        # output
+        self.fc_out = nn.Linear(resnet_dim, out_dim)
+
+    def forward(self, states_nnet):
+        x = states_nnet
+
+        # preprocess input
+        if self.one_hot_depth > 0:
+            x = F.one_hot(x.long(), self.one_hot_depth)
+            x = x.float()
+            x = x.view(-1, self.state_dim * self.one_hot_depth)
+        else:
+            x = x.float()
+
+        # first two hidden layers
+        x = self.fc1(x)
+        if self.batch_norm:
+            x = self.bn1(x)
+
+        x = F.relu(x)
+        x = self.fc2(x)
+        if self.batch_norm:
+            x = self.bn2(x)
+
+        x = F.relu(x)
+
+        # resnet blocks
+        for block_num in range(self.num_resnet_blocks):
+            res_inp = x
+            if self.batch_norm:
+                x = self.blocks[block_num][0](x)
+                x = self.blocks[block_num][1](x)
+                x = F.relu(x)
+                x = self.blocks[block_num][2](x)
+                x = self.blocks[block_num][3](x)
+            else:
+                x = self.blocks[block_num][0](x)
+                x = F.relu(x)
+                x = self.blocks[block_num][1](x)
+
+            x = F.relu(x + res_inp)
+
+        # output
+        x = self.fc_out(x)
         return x
+
+# class HeuristicModel(nn.Module):
+#     def __init__(self, input_dim):
+#         super(HeuristicModel, self).__init__()
+#         self.fc1 = nn.Linear(input_dim, 64)
+#         self.dropout1 = nn.Dropout(0.25)
+#         self.fc2 = nn.Linear(64, 32)
+#         self.dropout2 = nn.Dropout(0.25)
+#         self.fc3 = nn.Linear(32, 16)
+#         self.fc4 = nn.Linear(16, 1)
+#
+#     def forward(self, x):
+#         x = torch.relu(self.fc1(x))
+#         x = self.dropout1(x)
+#         x = torch.relu(self.fc2(x))
+#         x = self.dropout2(x)
+#         x = torch.relu(self.fc3(x))
+#         x = self.fc4(x)
+#         return x
 
 class LearnedHeuristic:
     def __init__(self, n=11, k=4):
         self._n = n
         self._k = k
-        self._model = HeuristicModel(n)
+        self._model = HeuristicModel(n, n**2, 5000, 1000, 4, 1, False)
         self._criterion = nn.MSELoss()
         self._optimizer = optim.Adam(self._model.parameters(), lr=0.001)
 
